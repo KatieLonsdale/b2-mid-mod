e212ab2e1e1e7ccd7f4b659d946bda8c1d8b3e7d
Katie Lonsdale <katie952@gmail.com>
2023-04-07 15:17:29 -0600

    test: department_worked_in method

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Employee, type: :model do
     it { should belong_to :department }
     it {should have_many :employee_tickets}
   end
+
+  describe "instance methods" do
+    before :each do
+      test_data
+    end
+    describe "department_worked_in" do
+      it 'returns the name of the department they belong to' do
+        expect(@employee_1.department_worked_in).to eq('IT')
+        expect(@employee_2.department_worked_in).to eq('IT')
+        expect(@employee_4.department_worked_in).to eq('Finance')
+      end
+    end
+  end
 end
