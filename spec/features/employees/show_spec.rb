07968948ac3208f79af163f50e8d43e3d1ab5f3b
Katie Lonsdale <katie952@gmail.com>
2023-04-07 16:17:29 -0600

    test: add specific test that tickets won't show up if not assigned to the employee

--- spec/features/employees/show_spec.rb
+++ spec/features/employees/show_spec.rb
@@ -33,20 +33,40 @@ RSpec.describe 'employees show page' do
     it "shows me all of their tickets from oldest to youngest" do
       visit "/employees/#{@employee_1.id}"
       within "#employee-tickets" do
-      expect(page).to have_content("Subject: Email")
-      expect(page).to have_content("Age: 2")
-      expect(page).to have_content("Subject: Virus")
-      expect(page).to have_content("Age: 6")
-      expect(page).to have_content("Subject: Mouse")
-      expect(page).to have_content("Age: 5")
-      
-      expect(@ticket_3.subject).to appear_before(@ticket_4.subject)
-      expect(@ticket_4.subject).to appear_before(@ticket_1.subject)
-      
-      expect(page).to have_no_content("Subject: Monitor")
+        expect(page).to have_content("Subject: Email")
+        expect(page).to have_content("Age: 2")
+        expect(page).to have_content("Subject: Virus")
+        expect(page).to have_content("Age: 6")
+        expect(page).to have_content("Subject: Mouse")
+        expect(page).to have_content("Age: 5")
+        
+        expect(@ticket_3.subject).to appear_before(@ticket_4.subject)
+        expect(@ticket_4.subject).to appear_before(@ticket_1.subject)
+        
+        expect(page).to have_no_content("Subject: Monitor")
+      end
+
+      visit "/employees/#{@employee_4.id}"
+        expect(page).to have_no_content("Open tickets:")
     end
-    
-    visit "/employees/#{@employee_4.id}"
+
+    it "shows me their oldest ticket separately" do
+      visit "/employees/#{@employee_1.id}"
+      within "#employee-oldest-ticket" do
+        expect(page).to have_content("Oldest Ticket: Virus")
+      end
+
+      visit "/employees/#{@employee_4.id}"
+        expect(page).to have_no_content("Oldest Ticket:")
+    end
+
+    it "does not show any tickets that aren't assigned to the employee" do
+      visit "/employees/#{@employee_1.id}"
+      within "#employee-tickets" do
+        expect(page).to have_no_content("Subject: Monitor")
+        expect(page).to have_no_content("Subject: Password")
+      end
+      visit "/employees/#{@employee_4.id}"
       within "#employee-tickets" do
         expect(page).to have_no_content("Subject: Email")
         expect(page).to have_no_content("Age: 2")
@@ -58,16 +78,12 @@ RSpec.describe 'employees show page' do
       end
     end
 
-    it "shows me their oldest ticket separately" do
-      visit "/employees/#{@employee_1.id}"
-      within "#employee-oldest-ticket" do
-        expect(page).to have_content("Oldest Ticket: Virus")
-      end
-
-      visit "/employees/#{@employee_4.id}"
-      within "#employee-oldest-ticket" do
-        expect(page).to have_no_content("Oldest Ticket:")
-      end
-    end
+  
+      # and I see a form to add a ticket to this department.
+      # When I fill in the form with the id of a ticket that already exists in the database
+      # and I click submit
+      # Then I am redirected back to that employees show page
+      # and I see the ticket's subject now listed.
+      # (you do not have to test for sad path, for example if the id does not match an existing ticket.)
   end
 end
