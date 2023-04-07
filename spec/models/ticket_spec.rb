0e65223a6779ff62afa0b22858c22a2a1f618f06
Katie Lonsdale <katie952@gmail.com>
2023-04-07 14:34:06 -0600

    add tests for relationships

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Employee, type: :model do
+RSpec.describe Ticket do
   describe 'relationships' do
-    it { should belong_to :department }
     it {should have_many :employee_tickets}
   end
 end
