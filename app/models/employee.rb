f63f5abbb777bb405982f9b3cc5220e13e534375
Katie Lonsdale <katie952@gmail.com>
2023-04-07 15:38:37 -0600

    feat: add tickets_open method

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,13 @@
 class Employee < ApplicationRecord
   belongs_to :department
   has_many :employee_tickets
-  has_many :employees, through: :employee_tickets
+  has_many :tickets, through: :employee_tickets
 
   def department_worked_in
     department.name
+  end
+
+  def tickets_open
+    tickets
   end
 end
