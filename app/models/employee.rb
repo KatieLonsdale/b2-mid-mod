81ab04aeebd0922ab00b3805e449fd4daa0b34c5
Katie Lonsdale <katie952@gmail.com>
2023-04-07 15:17:44 -0600

    feat: add department_worked_in method

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,8 @@ class Employee < ApplicationRecord
   belongs_to :department
   has_many :employee_tickets
   has_many :employees, through: :employee_tickets
+
+  def department_worked_in
+    department.name
+  end
 end
