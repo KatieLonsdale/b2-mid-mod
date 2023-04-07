bf2beb91fb94d4287509b24b5602a303109275dd
Katie Lonsdale <katie952@gmail.com>
2023-04-07 14:57:25 -0600

    test: employees_in_dept test

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 RSpec.describe Department, type: :model do
   describe 'relationships' do
     it { should have_many :employees }
-    
+  end
+
+  describe 'instance methods' do
+    before :each do
+      test_data
+    end
+    describe '#employees_in_dept' do
+      it 'returns a sentence with employees in the department' do
+        expect(@department_1.employees_in_dept).to eq('Roger, Stacey, and Steve')
+        expect(@department_2.employees_in_dept).to eq('Rebecca and Ella')
+      end
+    end
   end
 end
