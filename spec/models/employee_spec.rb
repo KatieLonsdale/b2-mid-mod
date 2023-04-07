8adb29fce51bf8cc40f7582a4c991688c4d8b37d
Katie Lonsdale <katie952@gmail.com>
2023-04-07 15:38:22 -0600

    test: tickets_open method

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Employee, type: :model do
   describe 'relationships' do
     it { should belong_to :department }
     it {should have_many :employee_tickets}
+    it {should have_many :tickets}
   end
 
   describe "instance methods" do
@@ -15,6 +16,13 @@ RSpec.describe Employee, type: :model do
         expect(@employee_1.department_worked_in).to eq('IT')
         expect(@employee_2.department_worked_in).to eq('IT')
         expect(@employee_4.department_worked_in).to eq('Finance')
+      end
+    end
+    describe "tickets_open" do
+      it 'returns an array of tickets associated with the employee' do
+        expect(@employee_1.tickets_open).to eq([@ticket_1, @ticket_3, @ticket_4])
+        expect(@employee_3.tickets_open).to eq([@ticket_4, @ticket_5])
+        expect(@employee_2.tickets_open).to eq([])
       end
     end
   end
